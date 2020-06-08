43f587768716d6676ea9dbaf5b017794f9f24cf2
Leonardo Freire <lcastrofreire@gmail.com>
2020-06-08 04:03:18 -0300

    chore: Adicionada uma base URL feat: Adicionada uma classe para teste na LP style: indentação do código

--- cypress/integration/teste_lp.spec.js
+++ cypress/integration/teste_lp.spec.js
@@ -2,7 +2,6 @@
 
 import {getDados} from '../support/util/getdados';
 
-
 const dados = getDados();
 
 context('Suite de teste do Tim Pre', () => {
@@ -11,37 +10,42 @@ context('Suite de teste do Tim Pre', () => {
         
         before(() => {
 
-          const sku =  'SVAP027RJ';           
-          cy.visit(dados.urlModal + sku);
-          console.log("acessando a pagina: " + dados.urlModal + sku)
+            cy.visit("/");
 
         });
 
+        it.only('Validando a LP', () =>{
+            
+            cy.contains('p', 'Aproveite todas as vantagens de ser TIM Pré Top')
+            
+          })
+
         it('Validando a tela de ativação', () =>{
-          cy.ValidaTelaAtivação();
-        })
+
+            cy.ValidaTelaAtivação();
+        });
 
         it('Preenchendo dados', () => {
 
-          cy.Preenchedados(dados.ddd,dados.telefone,dados.email);
+            cy.Preenchedados(dados.ddd,dados.telefone,dados.email);
 
         });
 
         it('Validando tela de SMS', () => {
 
-          cy.ValidaTelaSMS(dados.ddd,dados.telefone);
+            cy.ValidaTelaSMS(dados.ddd,dados.telefone);
 
         });
 
         it('Preenchendo código SMS', () => {
 
-          cy.PreencheSMS(dados.sms);
+            cy.PreencheSMS(dados.sms);
          
         });  
       
         it('Validando tela de parabéns', () => {
 
-          cy.ValidaParabens();
+            cy.ValidaParabens();
 
         });  
     });
