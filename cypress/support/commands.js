79023ac077233e968ec3e9c70e58b41ab78af583
Leonardo Freire <lcastrofreire@gmail.com>
2020-06-12 17:15:51 -0300

    feat: Criado método para escolha e validação do plano

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -45,4 +45,117 @@ Cypress.Commands.add("ValidaParabens", () => {
     .should('contain.text', 'Seu TIM PRÉ TOP foi ativado com sucesso!')
     .and('contain.text', 'Parabéns! Em instantes você receberá um SMS confirmando a ativação da sua oferta.');
 });
+
+Cypress.Commands.add("ValidaPlanosLP", (plano) => {
+
+    cy.get('li.price-bar-module--item--3ljpg').as('listaplanos')
+    .first().should('have.text', 'R$10').and('be.visible').as('plano10')
+    .next().should('have.text', 'R$15').and('be.visible').as('plano15')
+    .next().should('have.text', 'R$20').and('be.visible').as('plano20')
+    .next().should('have.text', 'R$30').and('be.visible').as('plano30')
+    .next().should('have.text', 'R$40').and('be.visible').as('plano40')
+    .next().should('have.text', 'R$50').and('be.visible').as('plano50')
+    .next().should('have.text', 'R$60').and('be.visible').as('plano60')
+    .next().should('have.text', 'R$100').and('be.visible').as('plano100');
+    
+    if (plano == "R$10") {
+
+        cy.get("@plano10").click();
+
+        cy.get('div.card-module--header--35edM').each(element => {
+            element.text()
+        }).should('have.text', `Recarregue${plano}E tenha por1 semana`);
+
+        cy.get('div.card-module--internetWrapper--cSeKC').as('qtdGB');
+        cy.get("@qtdGB").should('be.visible').and('have.text','1GB');
+        
+    }
+    if (plano == "R$15") {
+
+        cy.get("@plano15").click();
+
+        cy.get('div.card-module--column--3eUX9').each(element => {
+            element.show().text();
+        }).should('have.text', `Recarregue${plano}E tenha por10 dias`);
+
+        cy.get('div.card-module--internetWrapper--cSeKC').as('qtdGB');
+        cy.get("@qtdGB").should('be.visible').and('have.text','1,4GB');
+        
+    }
+    if (plano == "R$20") {
+
+        cy.get("@plano20").click();
+
+        cy.get('div.card-module--column--3eUX9').each(element => {
+            element.show().text();
+        }).should('have.text', `Recarregue${plano}E tenha por2 semanas`);
+
+        cy.get('div.card-module--internetWrapper--cSeKC').as('qtdGB');
+        cy.get("@qtdGB").should('be.visible').and('have.text','2GB');
+        
+    }
+    if (plano == "R$30") {
+
+        cy.get("@plano30").click();
+
+        cy.get('div.card-module--column--3eUX9').each(element => {
+            element.show().text();
+        }).should('have.text', `Recarregue${plano}E tenha por3 semanas`);
+
+        cy.get('div.card-module--internetWrapper--cSeKC').as('qtdGB');
+        cy.get("@qtdGB").should('be.visible').and('have.text','3GB');
+        
+    }
+    if (plano == "R$40") {
+
+        cy.get("@plano40").click();
+
+        cy.get('div.card-module--column--3eUX9').each(element => {
+            element.show().text();
+        }).should('have.text', `Recarregue${plano}E tenha por4 semanas`);
+
+        cy.get('div.card-module--internetWrapper--cSeKC').as('qtdGB');
+        cy.get("@qtdGB").should('be.visible').and('have.text','4GB');
+        
+    }
+    if (plano == "R$50") {
+
+        cy.get("@plano50").click();
+
+        cy.get('div.card-module--column--3eUX9').each(element => {
+            element.show().text();
+        }).should('have.text', `Recarregue${plano}E tenha por30 dias`);
+
+        cy.get('div.card-module--internetWrapper--cSeKC').as('qtdGB');
+        cy.get("@qtdGB").should('be.visible').and('have.text','4,3GB');
+        
+    }
+    if (plano == "R$60") {
+
+        cy.get("@plano60").click();
+
+        cy.get('div.card-module--column--3eUX9').each(element => {
+            element.show().text();
+        }).should('have.text', `Recarregue${plano}E tenha por30 dias`);
+
+        cy.get('div.card-module--internetWrapper--cSeKC').as('qtdGB');
+        cy.get("@qtdGB").should('be.visible').and('have.text','4,3GB');
+        
+    }
+    if (plano == "R$100") {
+
+        cy.get("@plano100").click();
+
+        cy.get('div.card-module--column--3eUX9').each(element => {
+            element.show().text();
+        }).should('have.text', `Recarregue${plano}E tenha por30 dias`);
+
+        cy.get('div.card-module--internetWrapper--cSeKC').as('qtdGB');
+        cy.get("@qtdGB").should('be.visible').and('have.text','4,3GB');
+        
+    }
+
+    cy.get('button').contains('Contratar agora').click();
+
+});
  
