8a06f93a0058d9922829ad5a9bbbc55775d2c5ed
Leonardo Freire <lcastrofreire@gmail.com>
2020-06-13 15:59:49 -0300

    feat: Adicionado pacote para controle de iframe feat: Finalizado fluxo de teste através da LP

--- cypress/integration/teste_lp.spec.js
+++ cypress/integration/teste_lp.spec.js
@@ -24,38 +24,55 @@ context('Suite de teste do Tim Pre', () => {
         
         it('Validando a escolha do plano', () =>{
 
-            cy.ValidaPlanosLP("R$15"); // Verifica se todos os planos estão disponiveis e valida os dados/informações do plano escolhido
+            cy.ValidaPlanosLP("R$30"); // Verifica se todos os planos estão disponiveis,valida os dados/informações do plano escolhido e realiza a contratação do mesmo
            
-          });
+        });
 
-        /* it('Validando a tela de ativação', () =>{
+        it('Validando a tela de ativação', () =>{
 
-            cy.ValidaTelaAtivação();
+            cy.frameLoaded('#modal-tim-pre')
+            cy.iframe('#modal-tim-pre').find('h1').should('have.text', 'Ative agora seuTIM Pré TOP NACIONAL').and('be.visible');
+
         });
 
         it('Preenchendo dados', () => {
 
-            cy.Preenchedados(dados.ddd,dados.telefone,dados.email);
+            cy.iframe('#modal-tim-pre').find('input[id=dddServico]').should('be.visible').type(dados.ddd);
+            cy.iframe('#modal-tim-pre').find('input[id=linhaServico]').should('be.visible').type(dados.telefone);
+            cy.iframe('#modal-tim-pre').find('input[id=email]').should('be.visible').type(dados.email);
+            cy.iframe('#modal-tim-pre').find('[type=submit]').should('be.visible').and('contain.text', 'Ativar').click();
 
         });
 
         it('Validando tela de SMS', () => {
 
-            cy.ValidaTelaSMS(dados.ddd,dados.telefone);
+            cy.iframe('#modal-tim-pre').find('p.title')
+            .should('have.text', 'Verifique em seucelular o sms como código desegurança').and('be.visible');
+
+            cy.iframe('#modal-tim-pre').find('p.description')
+            .should('be.visible').and('have.text',`Enviamos um SMS para o número (${dados.ddd}) ${dados.telefone} com um código de segurança.`);
 
         });
 
         it('Preenchendo código SMS', () => {
 
-            cy.PreencheSMS(dados.sms);
+            cy.iframe('#modal-tim-pre').find('input[name=token]')
+            .should('be.visible').and('have.attr', 'placeholder', 'X X X X')
+            .type(dados.sms);
+
+            cy.iframe('#modal-tim-pre').find('[type=submit]')
+            .should('be.visible').and('have.text', 'Ativar')
+            .click();
          
         });  
       
         it('Validando tela de parabéns', () => {
 
-            cy.ValidaParabens();
+            cy.iframe('#modal-tim-pre').find('div.congratulations-wrapper')
+            .and('contain.text', 'Seu TIM PRÉ TOP foi ativado com sucesso!')
+            .and('contain.text', 'Parabéns! Em instantes você receberá um SMS confirmando a ativação da sua oferta.');
 
-        });   */
+        });  
     });
 
 })
